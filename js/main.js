8cb69b6454b457ab6c256ff2e27a3775c60857bf
SOCHEATEK.MAO <SOCHEATEK.MAO@pnc.lan>
2023-12-17 12:47:17 +0700

    #7 push code display product to dashboard page

--- js/main.js
+++ js/main.js
@@ -13,4 +13,51 @@ function loadProducts() {
   }
 }
 
+let tbody = document.querySelector("tbody");
+
+function renderProducts() {
+
+  let productsStorage = JSON.parse(localStorage.getItem("products"));
+  if (productsStorage !== null) {
+    products = productsStorage;
+    for (let i = 0; i < products.length; i++) {
+      let product = products[i];
+      let tableRow = document.createElement('tr');
+      
+      let tdID = document.createElement('td');
+      tdID.textContent = i + 1;
+      
+      let tdName = document.createElement('td');
+      tdName.textContent = product.name;
+      
+      let tdCategory = document.createElement('td');
+      tdCategory.textContent = product.category;
+      
+      let tdPrice = document.createElement('td');
+      tdPrice.textContent = product.price;
+      
+      let tdAmount = document.createElement('td');
+      tdAmount.textContent = product.qty;
+
+      let tdProgress = document.createElement('td');
+      let ProgressUP  = document.createElement('img');
+      ProgressUP.src ='../IMG/up.png';
+      tdProgress.appendChild(ProgressUP);
+      
+      tableRow.appendChild(tdID);
+      tableRow.appendChild(tdName);
+      tableRow.appendChild(tdCategory);
+      tableRow.appendChild(tdPrice);
+      tableRow.appendChild(tdAmount);
+
+      tableRow.appendChild(tdProgress);
+      
+
+      
+      tbody.appendChild(tableRow);
+    }
+  }
+}
+
+renderProducts();
 loadProducts();
